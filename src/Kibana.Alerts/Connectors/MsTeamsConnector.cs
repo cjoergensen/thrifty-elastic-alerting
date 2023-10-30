5d640e219731303ab25e5bfaee21b41016263ea8
Christian Grønbæk Jørgensen <cjoergensen@live.com>
2023-10-30 16:36:01 +0100

    Added default MessageCardJson

--- src/Kibana.Alerts/Connectors/MsTeamsConnector.cs
+++ src/Kibana.Alerts/Connectors/MsTeamsConnector.cs
@@ -14,13 +14,40 @@ public sealed class MsTeamsConnector(IConfiguration configuration, HttpClient ht
     private readonly IConfiguration configuration = configuration;
     private readonly HttpClient httpClient = httpClient;
     private const string MediaType = "application/json";
+    private const string DefaultMessageCardJson = """
+    {
+      "type": "AdaptiveCard",
+      "body": [
+        {
+          "type": "TextBlock",
+          "size": "Medium",
+          "weight": "Bolder",
+          "text": "⚠ Alert Status Update - {{Name}} has changed state to: {{ExecutionStatus.Status}} ⚠"
+        },
+        {
+          "type": "TextBlock",
+          "text": "There has been a change in the status of alert "<strong>{{Name}}</strong>." It is now <strong>{{ExecutionStatus.Status}}</strong>.</p><p>For detailed information regarding this alert's status change, please click the view details button.",
+          "wrap": true
+        }
+      ],
+      "actions": [
+        {
+          "type": "Action.OpenUrl",
+          "title": "View details",
+          "url": "{{RuleUrl}}"
+        }
+      ],
+      "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+      "version": "1.6"
+    }
+    """;
 
     public async Task Send(Alert alert, IConfigurationSection configurationSection, CancellationToken cancellationToken = default)
     {
         var settings = new MsTeamsSettings();
         configurationSection.Bind(settings);
 
-        var cardTemplate = handlebars.Compile(settings.MessageCardJson);
+        var cardTemplate = handlebars.Compile(settings.MessageCardJson ?? DefaultMessageCardJson);
         var card = cardTemplate(alert);
 
         httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaType));
