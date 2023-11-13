33d480c43c2d9de8eba9316dc9c8debf36a98f0c
Peter Andersen <pa@truecon.dk>
2023-11-13 14:56:56 +0100

    Added initial

---
+++ src/Kibana.Alerts/Repositories/ElasticUserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Nest;
+using Elasticsearch.Net;
+
+namespace Kibana.Alerts.Repositories;
+public interface IUserRepository
+{
+    void UpdateUser();
+}
+internal class ElasticUserRepository : IUserRepository
+{
+    ElasticClient client;
+    public ElasticUserRepository(IConfiguration configuration)
+    {
+        var url = configuration["Elastic:Url"];
+        var username = configuration["Elastic:UserName"];
+        var password = configuration["Elastic:Password"];
+        client = Extensions.CreateClient(url, username, password);
+    }
+    public void UpdateUser()
+    {
+        client.LowLevel.DoRequest<StringResponse>(
+            Elasticsearch.Net.HttpMethod.PUT,
+            "_security/user/alerting",
+            PostData.Serializable(new
+            {
+                password = "a13rtin9",
+                roles = new List<string> { "kibana_admin" }
+            }));
+    }
+}
